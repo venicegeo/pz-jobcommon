5ae0e0f1b7606cdf35aa9933c2c355bd8deadf65
GitHubRGI <patrick.doody@rgi-corp.com>
2015-12-21 13:16:13 -0500

    Serialize DateTime to ISO8601

--- src/main/java/model/job/Job.java
+++ src/main/java/model/job/Job.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.joda.time.DateTime;
 import org.mongojack.ObjectId;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -14,6 +15,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Job {
 	public String jobId;
 	public PiazzaJobType jobType;
+	@JsonIgnore
 	public DateTime submitted;
 	public String submitterApiKey;
 	public String status;
@@ -31,5 +33,16 @@ public class Job {
 	@JsonProperty("_id")
 	public void setJobId(String jobId) {
 		this.jobId = jobId;
+	}
+
+	@JsonProperty("submitted")
+	public String getSubmittedString() {
+		// Defaults to ISO8601
+		return submitted.toString();
+	}
+
+	@JsonProperty("submitted")
+	public void setSubmittedString(String submitted) {
+		this.submitted = new DateTime(submitted);
 	}
 }
