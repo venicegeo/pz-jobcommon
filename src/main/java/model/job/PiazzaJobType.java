3b7c67e4289a0f3362c167deb7b5e6960ca172e4
GitHubRGI <patrick.doody@rgi-corp.com>
2015-12-29 12:36:11 -0500

    Ignoring unknown JSON properties

--- src/main/java/model/job/PiazzaJobType.java
+++ src/main/java/model/job/PiazzaJobType.java
@@ -4,9 +4,10 @@ import model.job.type.AbortJob;
 import model.job.type.GetJob;
 import model.job.type.IngestJob;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
@@ -14,6 +15,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonSubTypes({ @Type(value = GetJob.class, name = "get"), @Type(value = AbortJob.class, name = "abort"),
 		@Type(value = IngestJob.class, name = "ingest") })
 @JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=true)
 public interface PiazzaJobType {
 	public String getType();
 }
