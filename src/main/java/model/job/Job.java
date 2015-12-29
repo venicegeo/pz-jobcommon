09fbbbdaa96b965531192d65e2afd2663fd2af09
GitHubRGI <patrick.doody@rgi-corp.com>
2015-12-29 08:44:39 -0500

    Ignoring MongoDB implicit _id Field

--- src/main/java/model/job/Job.java
+++ src/main/java/model/job/Job.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.joda.time.DateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Job {
 	public String jobId;
 	public PiazzaJobType jobType;
