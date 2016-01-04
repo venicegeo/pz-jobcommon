8d60da1af6088d7d8866f86058d0b2bc63bb2cdf
GitHubRGI <patrick.doody@rgi-corp.com>
2016-01-04 15:53:23 -0500

    Temporarily Remove JodaTime

--- src/main/java/model/job/JobProgress.java
+++ src/main/java/model/job/JobProgress.java
@@ -1,11 +1,7 @@
 package model.job;
 
-import org.joda.time.DateTime;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Represents the progress of a Job. Job Workers can fire update to the progress
@@ -18,10 +14,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonInclude(Include.NON_NULL)
 public class JobProgress {
 	public Integer percentComplete;
-	@JsonIgnore
-	public DateTime timeRemaining;
-	@JsonIgnore
-	public DateTime timeSpent;
+	public String timeRemaining;
+	public String timeSpent;
 
 	public JobProgress() {
 	}
@@ -30,48 +24,7 @@ public class JobProgress {
 		this.percentComplete = percentComplete;
 	}
 
-	public JobProgress(DateTime timeSpent, DateTime timeRemaining) {
-		this.timeSpent = timeSpent;
-		this.timeRemaining = timeRemaining;
-	}
-
-	public JobProgress(Integer percentComplete, DateTime timeSpent, DateTime timeRemaining) {
-		this.percentComplete = percentComplete;
-		this.timeSpent = timeSpent;
-		this.timeRemaining = timeRemaining;
-	}
-
 	public Integer getPercentComplete() {
 		return percentComplete;
-	}
-
-	public DateTime getTimeRemaining() {
-		return timeRemaining;
-	}
-
-	public DateTime getTimeSpent() {
-		return timeSpent;
-	}
-
-	@JsonProperty("timeRemaining")
-	public String getTimeRemainingString() {
-		// Defaults to ISO8601
-		return timeRemaining.toString();
-	}
-
-	@JsonProperty("timeRemaining")
-	public void setTimeRemainingString(String timeRemaining) {
-		this.timeRemaining = new DateTime(timeRemaining);
-	}
-
-	@JsonProperty("timeSpent")
-	public String getTimeSpentString() {
-		// Defaults to ISO8601
-		return timeSpent.toString();
-	}
-
-	@JsonProperty("timeSpent")
-	public void setTimeSpentString(String timeSpent) {
-		this.timeSpent = new DateTime(timeSpent);
 	}
 }
