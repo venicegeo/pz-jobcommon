ded563469f08351343707d4e871f40a01b863ff1
marge <mlynum@radiantblue.com>
2016-01-07 10:41:21 -0500

    [FEATURE-587]Updated to add additional piazza types

--- src/main/java/model/job/PiazzaJobType.java
+++ src/main/java/model/job/PiazzaJobType.java
@@ -1,8 +1,13 @@
 package model.job;
 
 import model.job.type.AbortJob;
+import model.job.type.DeleteServiceJob;
+import model.job.type.ExecuteServiceJob;
 import model.job.type.GetJob;
 import model.job.type.IngestJob;
+import model.job.type.ReadServiceMetadataJob;
+import model.job.type.RegisterServiceJob;
+import model.job.type.UpdateServiceJob;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -12,8 +17,10 @@ import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({ @Type(value = GetJob.class, name = "get"), @Type(value = AbortJob.class, name = "abort"),
-		@Type(value = IngestJob.class, name = "ingest") })
+@JsonSubTypes({  @Type(value = AbortJob.class, name = "abort"),@Type(value= DeleteServiceJob.class, name = "delete-service"),
+				 @Type(value = ExecuteServiceJob.class, name = "execute-service"), @Type(value = GetJob.class, name = "get"), @Type(value = IngestJob.class, name = "ingest"), 
+		         @Type(value = ReadServiceMetadataJob.class, name = "read-service"), @Type(value = RegisterServiceJob.class, name = "register-service"),
+		         @Type(value = UpdateServiceJob.class, name = "update-service")})
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown=true)
 public interface PiazzaJobType {
