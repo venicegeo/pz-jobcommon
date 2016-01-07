352a85c3345ee4355522a28a4072377fa8f1886f
marge <mlynum@radiantblue.com>
2016-01-07 13:16:14 -0500

    [FEATURE-587] Updated to add ResourceMetadata for Ingest and Service

---
+++ src/main/java/model/job/metadata/ResourceMetadata.java
@@ -0,0 +1,31 @@
+package model.job.metadata;
+
+
+import org.joda.time.DateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class ResourceMetadata {
+	
+	public String name;
+	public String serviceID;
+	public String description;
+	public String url;
+	public String networks;
+	public String qos;
+	public String availability;
+	public String tags;
+	public String classType;
+	@JsonIgnore
+	public DateTime termDate;
+	public boolean clientCertRequired;
+	public boolean credentialsRequired;
+	public boolean preAuthRequired;
+    public String contacts;
+    public String method;
+    public String mimeType;
+    public String params;
+    public String reason;
+	
+
+}
