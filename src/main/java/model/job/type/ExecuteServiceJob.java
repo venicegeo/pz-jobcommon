2ec7b6fd0d38eea4e51de50edbfdb3788db079da
marge <mlynum@radiantblue.com>
2016-01-15 06:04:39 -0500

    [Feature-318]Extracted out executeService metadata into another class to leverage within servicecontroller for direct rest calls to execute

--- src/main/java/model/job/type/ExecuteServiceJob.java
+++ src/main/java/model/job/type/ExecuteServiceJob.java
@@ -1,44 +1,14 @@
 package model.job.type;
 
-import java.util.Map;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import model.job.PiazzaJobType;
+import model.job.metadata.ExecuteServiceData;
 
 public class ExecuteServiceJob implements PiazzaJobType {
 
 		public String jobId = null;
-		@JsonProperty("dataInputs")
-		public Map<String, String> dataInputs;	
-		public String dataInput;
-		public String resourceId;
 		public final String type = "execute-service";
-		
-		public Map<String, String> getDataInputs() {
-			return dataInputs;
-		}
+		public ExecuteServiceData data;
 
-		public void setDataInputs(Map<String, String> dataInputs) {
-			this.dataInputs = dataInputs;
-		}
-
-		public String getDataInput() {
-			return dataInput;
-		}
-
-		public void setDataInput(String dataInput) {
-			this.dataInput = dataInput;
-		}
-
-		public String getResourceId() {
-			return resourceId;
-		}
-
-		public void setResourceId(String resourceId) {
-			this.resourceId = resourceId;
-		}		
-		
 		public ExecuteServiceJob() {
 
 		}
