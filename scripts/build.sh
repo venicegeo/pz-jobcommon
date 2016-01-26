599be81bf9f72f5f8a1f06340fcb269764dc5db0
bkrasner <bkrasner@radiantblue.com>
2016-01-26 15:20:45 -0500

    added scripts

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+base=$(pwd -P)
+popd > /dev/null
+
+# Build Spring-boot JAR
+mvn clean package
+
+# Path to output JAR
+jarfile=$base/../target/pz-jobcommon-1.0.0.BUILD-SNAPSHOT.jar
+
