78052aeab6de96fa8f19bce907f73d79a5c9b5f5
bkrasner <bkrasner@radiantblue.com>
2016-01-26 15:47:27 -0500

    simplify

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -ex
-pwd
-pushd `dirname $0` > /dev/null
-base=$(pwd -P)
-popd > /dev/null
 
-# Build Spring-boot JAR
 mvn clean package
 
 # Path to output JAR
-jarfile=$base/../target/pz-jobcommon-1.0.0.BUILD-SNAPSHOT.jar
-
+jarfile=./target/pz-jobcommon-1.0.0.BUILD-SNAPSHOT.jar
