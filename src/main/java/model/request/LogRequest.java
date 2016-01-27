0b24b78c1f0c0975bf682f26985b8e0489dbd245
bkrasner <bkrasner@radiantblue.com>
2016-01-27 11:59:20 -0500

    added getters amd setters

--- src/main/java/model/request/LogRequest.java
+++ src/main/java/model/request/LogRequest.java
@@ -6,5 +6,36 @@ public class LogRequest {
 	String time;
 	String message;
 	String severity;
+	public String getService() {
+		return service;
+	}
+	public void setService(String service) {
+		this.service = service;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getSeverity() {
+		return severity;
+	}
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+	
 
 }
