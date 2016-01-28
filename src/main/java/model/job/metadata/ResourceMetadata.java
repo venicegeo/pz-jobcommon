ed9eb25a1085f4a7afe61061b7aac9c854be3ede
GitHubRGI <patrick.doody@rgi-corp.com>
2016-01-28 11:59:19 -0500

    Updating properties; avoid serialize null props

--- src/main/java/model/job/metadata/ResourceMetadata.java
+++ src/main/java/model/job/metadata/ResourceMetadata.java
@@ -1,22 +1,25 @@
 package model.job.metadata;
 
-
 import java.util.List;
 
 import org.joda.time.DateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonInclude(Include.NON_NULL)
 public class ResourceMetadata {
-	
+
 	public String name;
 
-	@JsonProperty("_id") 
+	@JsonProperty("_id")
 	public String resourceId;
 	public String description;
 	public String url;
 	public String filePath;
+	public String format;
 	public String networks;
 	public String qos;
 	public String availability;
@@ -27,13 +30,13 @@ public class ResourceMetadata {
 	public boolean clientCertRequired;
 	public boolean credentialsRequired;
 	public boolean preAuthRequired;
-    public String contacts;
-    public String method;
-    public String requestMimeType;
-    public String param;
-    @JsonProperty("params")
-    public List <String> params;
-    public String responseMimeType;
-    public String reason;
+	public String contacts;
+	public String method;
+	public String requestMimeType;
+	public String param;
+	@JsonProperty("params")
+	public List<String> params;
+	public String responseMimeType;
+	public String reason;
 
 }
