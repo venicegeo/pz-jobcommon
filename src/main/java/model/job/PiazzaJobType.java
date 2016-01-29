17b4b7a7a120eecb3800460570c6775cf32c2f21
bkrasner <bkrasner@radiantblue.com>
2016-01-29 12:14:43 -0500

    Rename ReadServiceMetadataJob to DescribeServiceMetadataJob

--- src/main/java/model/job/PiazzaJobType.java
+++ src/main/java/model/job/PiazzaJobType.java
@@ -6,7 +6,7 @@ import model.job.type.ExecuteServiceJob;
 import model.job.type.GetJob;
 import model.job.type.IngestJob;
 import model.job.type.ListServicesJob;
-import model.job.type.ReadServiceMetadataJob;
+import model.job.type.DescribeServiceMetadataJob;
 import model.job.type.RegisterServiceJob;
 import model.job.type.UpdateServiceJob;
 
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({  @Type(value = AbortJob.class, name = "abort"),@Type(value= DeleteServiceJob.class, name = "delete-service"),
 				 @Type(value = ExecuteServiceJob.class, name = "execute-service"), @Type(value = GetJob.class, name = "get"), @Type(value = IngestJob.class, name = "ingest"), 
-		         @Type(value = ReadServiceMetadataJob.class, name = "read-service"), @Type(value = RegisterServiceJob.class, name = "register-service"),
+		         @Type(value = DescribeServiceMetadataJob.class, name = "read-service"), @Type(value = RegisterServiceJob.class, name = "register-service"),
 		         @Type(value = UpdateServiceJob.class, name = "update-service"),
 		         @Type(value = ListServicesJob.class, name = "list-service")})
 @JsonInclude(Include.NON_NULL)
