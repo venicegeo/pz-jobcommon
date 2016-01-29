255dc5fb902bd538dcfc46732cc48391c5f5e156
GitHubRGI <patrick.doody@rgi-corp.com>
2016-01-29 16:23:54 -0500

    Adding status consts

--- src/main/java/model/status/StatusUpdate.java
+++ src/main/java/model/status/StatusUpdate.java
@@ -2,6 +2,7 @@ package model.status;
 
 import model.job.JobProgress;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -14,8 +15,29 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
  */
 @JsonInclude(Include.NON_NULL)
 public class StatusUpdate {
-	public String status;
-	public JobProgress progress;
+	@JsonIgnore
+	public static final String STATUS_SUBMITTED = "Submitted";
+	@JsonIgnore
+	public static final String STATUS_PENDING = "Pending";
+	@JsonIgnore
+	public static final String STATUS_RUNNING = "Running";
+	@JsonIgnore
+	public static final String STATUS_SUCCESS = "Success";
+	@JsonIgnore
+	public static final String STATUS_CANCELLED = "Cancelled";
+	@JsonIgnore
+	public static final String STATUS_COMPLETE = "Complete";
+	@JsonIgnore
+	public static final String STATUS_ERROR = "Error";
+	@JsonIgnore
+	public static final String STATUS_FAIL = "Fail";
+
+	private String status;
+	private JobProgress progress;
+
+	public StatusUpdate() {
+
+	}
 
 	public StatusUpdate(String status) {
 		this.status = status;
@@ -30,7 +52,15 @@ public class StatusUpdate {
 		return status;
 	}
 
-	public JobProgress getJobProgress() {
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public JobProgress getProgress() {
 		return progress;
+	}
+
+	public void setProgress(JobProgress progress) {
+		this.progress = progress;
 	}
 }
