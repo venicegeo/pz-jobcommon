8f40787a12648979f5621e72d9403ec604abb6ce
GitHubRGI <patrick.doody@rgi-corp.com>
2016-02-08 13:51:44 -0500

    Fields for shapefile table name

--- src/main/java/model/data/type/ShapefileResource.java
+++ src/main/java/model/data/type/ShapefileResource.java
@@ -14,6 +14,7 @@ import model.data.location.FileLocation;
  */
 public class ShapefileResource implements DataType {
 	public static final String type = "shapefile";
+	public String databaseTableName;
 	public FileLocation location;
 
 	public ShapefileResource() {
@@ -24,11 +25,54 @@ public class ShapefileResource implements DataType {
 		return type;
 	}
 
+	/**
+	 * Gets the location of the raw, zipped shapefile that was sent to the
+	 * Gateway upon Ingest.
+	 * 
+	 * @return Shapefile location
+	 */
 	public FileLocation getLocation() {
 		return location;
 	}
 
+	/**
+	 * Sets the location of the raw, zipped shapefile that was sent to the
+	 * Gateway upon Ingest.
+	 * 
+	 * @param location
+	 *            The current shapefile location
+	 */
 	public void setLocation(FileLocation location) {
 		this.location = location;
+	}
+
+	/**
+	 * Gets the Piazza PostGIS table name that this Shapefile was inserted into
+	 * 
+	 * Shapefiles are more easily managed as PostGIS database tables, than as
+	 * flat shapefiles. Because of this, all Shapefiles that are ingested into
+	 * Piazza are then inserted into the internal Piazza PostGIS database. From
+	 * this point on, they are then referenced via this table.
+	 * 
+	 * @return Table name for this Resource
+	 */
+	public String getDatabaseTableName() {
+		return databaseTableName;
+	}
+
+	/**
+	 * Sets the table name for the Piazza PostGIS database that holds this
+	 * dataset.
+	 * 
+	 * Shapefiles are more easily managed as PostGIS database tables, than as
+	 * flat shapefiles. Because of this, all Shapefiles that are ingested into
+	 * Piazza are then inserted into the internal Piazza PostGIS database. From
+	 * this point on, they are then referenced via this table.
+	 * 
+	 * @param tableName
+	 *            The Table name in the database for this dataset.
+	 */
+	public void setDatabaseTableName(String databaseTableName) {
+		this.databaseTableName = databaseTableName;
 	}
 }
