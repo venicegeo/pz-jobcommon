cce073a6d864d977db1e3702179228f51011c64e
go0ty <patrick.doody@rgi-corp.com>
2016-03-22 07:44:19 -0400

    mvn fix for immediate dep fetching

--- ci/archive.sh
+++ ci/archive.sh
@@ -11,7 +11,7 @@ source $root/ci/vars.sh
 src=$root/target/pz-jobcommon-1.0.0.BUILD-SNAPSHOT.jar
 
 # Build Spring-boot JAR
-[ -f $src ] || mvn clean package
+[ -f $src ] || mvn clean package -U
 
 # stage the artifact for a mvn deploy
 mv $src $root/$APP.$EXT
