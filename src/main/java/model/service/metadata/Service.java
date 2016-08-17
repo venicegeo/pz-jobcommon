2b101f62496d8c49176c9de69d87d295f4fcb0ab
Patrick D <patrick.doody@rgi-corp.com>
2016-08-17 11:50:52 -0400

    5968 Merging Service Properties

--- src/main/java/model/service/metadata/Service.java
+++ src/main/java/model/service/metadata/Service.java
@@ -15,6 +15,14 @@
  **/
 package model.service.metadata;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -22,50 +30,46 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import io.swagger.annotations.ApiModelProperty;
 import model.job.metadata.ResourceMetadata;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 /**
  * Class which represents a service registered and managed by Piazza
+ * 
  * @author mlynum
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
 
 public class Service {
-	
+
 	public enum METHOD_TYPE {
 		GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
 	};
-	
+
 	@ApiModelProperty(value = "HTTP method types")
 	private METHOD_TYPE methodType;
-	
+
 	@ApiModelProperty(required = false, value = "Provided by the System. The unique Id of the Service")
 	private String serviceId;
 
 	@ApiModelProperty(required = true, value = "The URL to the user Service to be executed")
 	@NotNull
-	@Size(min=1)
+	@Size(min = 1)
 	private String url;
 
 	@ApiModelProperty(required = true, value = "URL to the schema or contract to interface with the Service, such as a Swagger file, or documentation")
 	@NotNull
-	@Size(min=1)
+	@Size(min = 1)
 	private String contractUrl;
 
 	@ApiModelProperty(required = true, value = "The HTTP method used to invoke this user Service", dataType = "string", allowableValues = "GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT")
 	@NotNull
-	@Size(min=1)
+	@Size(min = 1)
 	private String method;
-	
-	@ApiModelProperty(value = "The timeout Piazza waits for a response (defaults to 120 seconds)")
-    private Long timeout;
-	
-	@ApiModelProperty(value = "The frequency in which Piazza sends heartbeat requests to check on the health of the Service  (defaults to 120 seconds)")
-    private Long heartbeat;
 
+	@ApiModelProperty(value = "The timeout Piazza waits for a response (defaults to 120 seconds)")
+	private Long timeout;
+
+	@ApiModelProperty(value = "The frequency in which Piazza sends heartbeat requests to check on the health of the Service  (defaults to 120 seconds)")
+	private Long heartbeat;
 
 	@ApiModelProperty(value = "Object of common metadata fields used to describe Data or Services within the Piazza system")
 	@NotNull
@@ -79,7 +83,7 @@ public class Service {
 	public void setHeartbeat(Long heartbeat) {
 		this.heartbeat = heartbeat;
 	}
-	
+
 	public String getMethod() {
 		return method;
 	}
@@ -95,7 +99,7 @@ public class Service {
 	public void setServiceId(String serviceId) {
 		this.serviceId = serviceId;
 	}
-	
+
 	public Long getTimeout() {
 		return timeout;
 	}
@@ -103,6 +107,7 @@ public class Service {
 	public void setTimeout(Long timeout) {
 		this.timeout = timeout;
 	}
+
 	public ResourceMetadata getResourceMetadata() {
 		return resourceMetadata;
 	}
@@ -125,5 +130,71 @@ public class Service {
 
 	public void setContractUrl(String contractUrl) {
 		this.contractUrl = contractUrl;
+	}
+
+	public METHOD_TYPE getMethodType() {
+		return methodType;
+	}
+
+	public void setMethodType(METHOD_TYPE methodType) {
+		this.methodType = methodType;
+	}
+
+	/**
+	 * Merges the properties of another Service into this one.
+	 * 
+	 * <p>
+	 * If the other Service specifies properties, then those properties take precedence. If the other Service contains a
+	 * null value for a property that exists in this object, then this object's property is unchanged unless the
+	 * overwriteNull flag is set to true.
+	 * </p>
+	 * 
+	 * <p>
+	 * The ServiceId is locked and can never be overridden. The ResourceMetadata properties will follow the same
+	 * principals - only properties with legit values will be overridden.
+	 * </p>
+	 * 
+	 * @param other
+	 *            The Service properties to merge
+	 * @param overwriteNull
+	 *            True if null values in the other Service should overwrite values in this object. False if not.
+	 */
+	public void merge(Service other, boolean overwriteNull) {
+		// Protected method names that are not automatically transferred (Id is protected, and ResourceMetadata merging
+		// is delegated to that object.
+		List<String> protectedNames = new ArrayList<String>();
+		protectedNames.add("setResourceMetadata");
+		protectedNames.add("setServiceId");
+
+		Method[] methods = this.getClass().getMethods();
+
+		// Loop through every Property
+		for (Method fromMethod : methods) {
+			if (fromMethod.getDeclaringClass().equals(this.getClass()) && fromMethod.getName().startsWith("get")) {
+
+				String fromName = fromMethod.getName();
+				String toName = fromName.replace("get", "set");
+
+				if (protectedNames.contains(toName)) {
+					continue;
+				}
+
+				try {
+					Method toMethod = this.getClass().getMethod(toName, fromMethod.getReturnType());
+					Object value = fromMethod.invoke(other, (Object[]) null);
+					if ((value != null) || (overwriteNull == true)) {
+						toMethod.invoke(this, value);
+					}
+				} catch (Exception exception) {
+					exception.printStackTrace();
+				}
+			}
+		}
+
+		// ResourceMetadata handles its own merging.
+		if (getResourceMetadata() == null) {
+			this.setResourceMetadata(new ResourceMetadata());
+		}
+		this.resourceMetadata.merge(other.getResourceMetadata(), overwriteNull);
 	}
 }
