943619524f766d7dd5c8d376a3e9cc1551389977
Ben Hosmer <ben.hosmer@gmail.com>
2017-05-01 13:57:17 -0400

    Specify path to workspace .2

--- ci/sonar.sh
+++ ci/sonar.sh
@@ -2,4 +2,4 @@
 root=$(pwd -P)
 mkdir -p $root/.m2/repository
 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test org.jacoco:jacoco-maven-plugin:report -DdataFile=$root/target/jacoco.exec
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test org.jacoco:jacoco-maven-plugin:report -DdataFile=$root/target/jacoco.exec -Dmaven.repo.local="$root/.m2/repository"
