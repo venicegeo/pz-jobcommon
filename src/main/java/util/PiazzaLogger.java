1e2963b1dc441db50c8c6c102991aba0fd36a485
wkhunt <whunt@radiantblue.com>
2017-05-12 13:32:16 -0400

    Added es 5.4.0 to pom

--- src/main/java/util/PiazzaLogger.java
+++ src/main/java/util/PiazzaLogger.java
@@ -30,6 +30,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -109,8 +110,8 @@ public class PiazzaLogger {
 	 */
 	@Bean
 	public Client getClient() throws UnknownHostException {
-		Settings settings = Settings.settingsBuilder().put("cluster.name", clustername).build();
-		TransportClient transportClient = TransportClient.builder().settings(settings).build();
+		Settings settings = Settings.builder().put("cluster.name", clustername).build();
+		TransportClient transportClient = new PreBuiltTransportClient(settings);
 		// Check if the ES Host property has multiple Hosts.
 		if (elasticSearchHost.contains(";")) {
 			// (In the form of "host;host2;host3")
