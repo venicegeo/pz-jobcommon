bf7a62eed40ea0cda39c896aa78ff012682f131b
Patrick D <patrick.doody@rgi-corp.com>
2017-08-01 13:26:25 -0400

    Service Pagination Queries

--- src/main/java/org/venice/piazza/common/hibernate/dao/service/ServiceDaoImpl.java
+++ src/main/java/org/venice/piazza/common/hibernate/dao/service/ServiceDaoImpl.java
@@ -41,31 +41,30 @@ public class ServiceDaoImpl implements ServiceDaoCustom {
 	@PersistenceContext
 	EntityManager entityManager;
 
+	private static final String WILDCARD_STRING_QUERY = "%%%s%%";
 	private static final String SERVICE_QUERY = "select * from service order by data ->> ?1 %s limit ?2 offset ?3";
 	private static final String SERVICE_QUERY_COUNT = "select count(*) from service";
-	private static final String KEYWORD_SERVICE_QUERY = "select * from service where data->'resourceMetadata'->'name' like ?1 or data->'resourceMetadata'->'description' like ?2 order by data ->> ?3 %s limit ?4 offset ?5";
-	private static final String KEYWORD_SERVICE_QUERY_COUNT = "select count(*) from service where data->'resourceMetadata'->'name' like ?1 or data->'resourceMetadata'->'description' like ?2";
-	private static final String USERNAME_SERVICE_QUERY = "select * from service where data->'resourceMetadata'->'createdBy' = ?1 order by data ->> ?2 %s limit ?3 offset ?4";
-	private static final String USERNAME_SERVICE_QUERY_COUNT = "select count(*) from service where data->'resourceMetadata'->'createdBy' = ?1";
-	private static final String USERNAME_AND_KEYWORD_SERVICE_QUERY = "select * from service where data->'resourceMetadata'->'name' like ?1 or data->'resourceMetadata'->'description' like ?2 and where data->'resourceMetadata'->'createdBy' = ?3 order by data ->> ?4 %s limit ?5 offset ?6";
-	private static final String USERNAME_AND_KEYWORD_SERVICE_QUERY_COUNT = "select count(*) from service where data->'resourceMetadata'->'name' like ?1 or data->'resourceMetadata'->'description' like ?2 and where data->'resourceMetadata'->'createdBy' = ?3";
+	private static final String KEYWORD_SERVICE_QUERY = "select * from service where data#>>'{resourceMetadata,name}' like ?1 or data#>>'{resourceMetadata,description}' like ?2 order by data ->> ?3 %s limit ?4 offset ?5";
+	private static final String KEYWORD_SERVICE_QUERY_COUNT = "select count(*) from service where data#>>'{resourceMetadata,name}' like ?1 or data#>>'{resourceMetadata,description}' like ?2";
+	private static final String USERNAME_SERVICE_QUERY = "select * from service where data#>>'{resourceMetadata,createdBy}' like ?1 order by data ->> ?2 %s limit ?3 offset ?4";
+	private static final String USERNAME_SERVICE_QUERY_COUNT = "select count(*) from service where data#>>'{resourceMetadata,createdBy}' like ?1";
 
 	public Page<ServiceEntity> getServiceListForUserAndKeyword(String keyword, String userName, Pagination pagination) {
 		// Query
 		String queryString = String.format(USERNAME_AND_KEYWORD_SERVICE_QUERY, Direction.fromString(pagination.getOrder()));
 		Query query = entityManager.createNativeQuery(queryString, ServiceEntity.class);
-		query.setParameter(1, keyword);
-		query.setParameter(2, keyword);
-		query.setParameter(3, userName);
+		query.setParameter(1, String.format(WILDCARD_STRING_QUERY, keyword));
+		query.setParameter(2, String.format(WILDCARD_STRING_QUERY, keyword));
+		query.setParameter(3, String.format(WILDCARD_STRING_QUERY, userName));
 		query.setParameter(4, pagination.getSortBy());
 		query.setParameter(5, pagination.getPerPage());
 		query.setParameter(6, pagination.getPage() * pagination.getPerPage());
 		List<ServiceEntity> results = query.getResultList();
 		// Count
 		query = entityManager.createNativeQuery(USERNAME_AND_KEYWORD_SERVICE_QUERY_COUNT);
-		query.setParameter(1, keyword);
-		query.setParameter(2, keyword);
-		query.setParameter(3, userName);
+		query.setParameter(1, String.format(WILDCARD_STRING_QUERY, keyword));
+		query.setParameter(2, String.format(WILDCARD_STRING_QUERY, keyword));
+		query.setParameter(3, String.format(WILDCARD_STRING_QUERY, userName));
 		long count = ((BigInteger) query.getSingleResult()).longValue();
 		return new PageImpl<ServiceEntity>(results, null, count);
 	}
@@ -88,14 +87,14 @@ public class ServiceDaoImpl implements ServiceDaoCustom {
 		// Query
 		String queryString = String.format(USERNAME_SERVICE_QUERY, Direction.fromString(pagination.getOrder()));
 		Query query = entityManager.createNativeQuery(queryString, ServiceEntity.class);
-		query.setParameter(1, userName);
+		query.setParameter(1, String.format(WILDCARD_STRING_QUERY, userName));
 		query.setParameter(2, pagination.getSortBy());
 		query.setParameter(3, pagination.getPerPage());
 		query.setParameter(4, pagination.getPage() * pagination.getPerPage());
 		List<ServiceEntity> results = query.getResultList();
 		// Count
 		query = entityManager.createNativeQuery(USERNAME_SERVICE_QUERY_COUNT);
-		query.setParameter(1, userName);
+		query.setParameter(1, String.format(WILDCARD_STRING_QUERY, userName));
 		long count = ((BigInteger) query.getSingleResult()).longValue();
 		return new PageImpl<ServiceEntity>(results, null, count);
 	}
@@ -104,16 +103,16 @@ public class ServiceDaoImpl implements ServiceDaoCustom {
 		// Query
 		String queryString = String.format(KEYWORD_SERVICE_QUERY, Direction.fromString(pagination.getOrder()));
 		Query query = entityManager.createNativeQuery(queryString, ServiceEntity.class);
-		query.setParameter(1, keyword);
-		query.setParameter(2, keyword);
+		query.setParameter(1, String.format(WILDCARD_STRING_QUERY, keyword));
+		query.setParameter(2, String.format(WILDCARD_STRING_QUERY, keyword));
 		query.setParameter(3, pagination.getSortBy());
 		query.setParameter(4, pagination.getPerPage());
 		query.setParameter(5, pagination.getPage() * pagination.getPerPage());
 		List<ServiceEntity> results = query.getResultList();
 		// Count
 		query = entityManager.createNativeQuery(KEYWORD_SERVICE_QUERY_COUNT);
-		query.setParameter(1, keyword);
-		query.setParameter(2, keyword);
+		query.setParameter(1, String.format(WILDCARD_STRING_QUERY, keyword));
+		query.setParameter(2, String.format(WILDCARD_STRING_QUERY, keyword));
 		long count = ((BigInteger) query.getSingleResult()).longValue();
 		return new PageImpl<ServiceEntity>(results, null, count);
 	}
