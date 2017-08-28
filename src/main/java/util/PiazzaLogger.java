88cd0420dfebce336a28b4a56cfaa817a0c31c55
Patrick D <patrick.doody@rgi-corp.com>
2017-08-28 13:08:52 -0400

    Remove unused Imports

--- src/main/java/util/PiazzaLogger.java
+++ src/main/java/util/PiazzaLogger.java
@@ -25,7 +25,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
