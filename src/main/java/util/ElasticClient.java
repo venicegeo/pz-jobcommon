91549f32f22bbe462b095178add720e6c92340ec
Sonny Saniev <ssaniev@gmail.com>
2017-11-21 16:55:57 -0500

    Adding elastic authentication to client instantiation with x-pack-transport

--- src/main/java/util/ElasticClient.java
+++ src/main/java/util/ElasticClient.java
@@ -10,9 +10,11 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class ElasticClient {
@@ -22,6 +24,10 @@ public class ElasticClient {
 	private String elasticSearchHost;
 	@Value("${elasticsearch.clustername}")
 	private String clustername;
+	@Value("${elasticsearch.username}")
+	private String elasticUsername;
+	@Value("${elasticsearch.password}")
+	private String elasticPassword;
 
 	/**
 	 * Spring bean for injecting elasticsearch client.
@@ -31,12 +37,15 @@ public class ElasticClient {
 	 */
 	@Bean
 	public Client getClient() throws UnknownHostException {
-		Settings settings = Settings.builder().put("cluster.name", clustername).build();
-		TransportClient transportClient = new PreBuiltTransportClient(settings);
+		String elasticCredentials = String.format("%s:%s", elasticUsername, elasticPassword);
+		Settings settings = Settings.builder().put("cluster.name", clustername).put("xpack.security.user", elasticCredentials).build();
+
+		//TransportClient transportClient = new PreBuiltTransportClient(settings);
+		TransportClient transportClient = new PreBuiltXPackTransportClient(settings);
+				
 		// Check if the ES Host property has multiple Hosts.
 		if (elasticSearchHost.contains(";")) {
 			// (In the form of "host;host2;host3")
-			//
 			// Multiple hosts. Split the string and add each host.
 			List<String> hosts = Arrays.asList(elasticSearchHost.split(";"));
 			for (String host : hosts) {
@@ -46,8 +55,7 @@ public class ElasticClient {
 			// (A single host)
 			//
 			// Single host. Add this one host.
-			transportClient
-					.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(elasticSearchHost, elasticSearchPort)));
+			transportClient.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(elasticSearchHost, elasticSearchPort)));
 		}
 		return transportClient;
 	}
