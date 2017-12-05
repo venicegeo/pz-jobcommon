3fa2005dac0bcbf4551bcf18fd90be8a393ac353
Sonny Saniev <ssaniev@gmail.com>
2017-12-05 12:27:47 -0500

    parametrizing clustername using different version of xpack

--- src/main/java/util/ElasticClient.java
+++ src/main/java/util/ElasticClient.java
@@ -10,7 +10,7 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
+//import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,8 +24,6 @@ public class ElasticClient {
 	private Integer elasticSearchPort;
 	@Value("${vcap.services.pz-elasticsearch.credentials.host}")
 	private String elasticSearchHost;
-	@Value("${elasticsearch.manualhost}")
-	private String elasticSearchCustomHost;
 	@Value("${vcap.services.pz-elasticsearch.credentials.clusterId}")
 	private String clusterId;
 	@Value("${vcap.services.pz-elasticsearch.credentials.username}")
@@ -33,6 +31,11 @@ public class ElasticClient {
 	@Value("${vcap.services.pz-elasticsearch.credentials.password}")
 	private String elasticPassword;
 
+	@Value("${elasticsearch.manualhost}")
+	private String elasticSearchCustomHost;
+	@Value("${elasticsearch.manualclustername}")
+	private String manualClusterName;
+	
 	private final Logger LOGGER = LoggerFactory.getLogger(PiazzaLogger.class);
 	
 	/**
@@ -44,43 +47,27 @@ public class ElasticClient {
 	@Bean
 	public Client getClient() throws UnknownHostException {
 
-		LOGGER.info(" ================= " + String.format("elasticSearchPort: %s,  elasticSearchHost: %s, clustername: %s, elasticUsername: %s, elasticPassword: %s", elasticSearchPort, elasticSearchHost, clusterId, elasticUsername, elasticPassword));
-		
 		String elasticCredentials = String.format("%s:%s", elasticUsername, elasticPassword);
-//		Settings settings = Settings.builder()
-//				.put("cluster.name", clusterId)
-//				//.put("xpack.security.user", elasticCredentials)
-//				.put("shield.transport.ssl", true)
-//				.put("request.headers.X-Found-Cluster", clusterId)
-//				.put("shield.user", elasticCredentials) // your shield username and password
-//				.build();
 
-//		Settings settings = Settings.builder()
-//				.put("client.transport.nodes_sampler_interval", "5s")
-//				.put("cluster.name", clusterId)
-//		        .put("client.transport.sniff", false)
-//		        .put("transport.tcp.compress", true)
-//		        .put("xpack.security.transport.ssl.enabled", true)
-//				.put("xpack.security.user", elasticCredentials)
-//				.put("shield.transport.ssl", true)
-//				.put("request.headers.X-Found-Cluster", clusterId)
-//				.put("shield.user", elasticCredentials) // your shield username and password
-//				.build();
+		String clusterName = clusterId;
+		if(!"empty".equals(manualClusterName)){
+			clusterName = manualClusterName;
+		}
+		
 		
         // Build the settings for our client.
         Settings settings = Settings.builder()
             .put("client.transport.nodes_sampler_interval", "5s")
             .put("client.transport.sniff", false)
             .put("transport.tcp.compress", true)
-            .put("cluster.name", clusterId)
+            .put("cluster.name", clusterName)
             .put("xpack.security.transport.ssl.enabled", true)
-            .put("request.headers.X-Found-Cluster", clusterId)
+            .put("request.headers.X-Found-Cluster", clusterName)
             .put("xpack.security.user", elasticCredentials)
-            //.put("xpack.security.user", System.getProperty("xpack.security.user"))
             .build();
         
-		//TransportClient transportClient = new PreBuiltTransportClient(settings);
-		TransportClient transportClient = new PreBuiltXPackTransportClient(settings);
+		TransportClient transportClient = new PreBuiltTransportClient(settings);
+		//TransportClient transportClient = new PreBuiltXPackTransportClient(settings);
 				
 		// Check if the ES Host property has multiple Hosts.
 		if (elasticSearchHost.contains(";")) {
@@ -88,7 +75,7 @@ public class ElasticClient {
 			// Multiple hosts. Split the string and add each host.
 			List<String> hosts = Arrays.asList(elasticSearchHost.split(";"));
 			for (String host : hosts) {
-				//String hostName = String.format("%s.%s", clusterId, host);
+				//String hostName = String.format("%s.%s", clusterName, host);
 				
 				String hostName = host;
 				if(!"empty".equals(elasticSearchCustomHost)){
@@ -102,11 +89,14 @@ public class ElasticClient {
 			// (A single host)
 			//
 			// Single host. Add this one host.
-			//String hostName = String.format("%s.%s", clusterId, elasticSearchHost);
+			//String hostName = String.format("%s.%s", clusterName, elasticSearchHost);
 			String hostName = elasticSearchHost;
 			if(!"empty".equals(elasticSearchCustomHost)){
 				hostName = elasticSearchCustomHost;
 			}
+
+			LOGGER.info(" ================= " + String.format("elasticSearchPort: %s,  elasticSearchHost: %s, clustername: %s, elasticUsername: %s, elasticPassword: %s", elasticSearchPort, elasticSearchHost, clusterName, elasticUsername, elasticPassword));
+			
 			LOGGER.info(" ================= " + " adding a single --host: " + hostName + " --Port: " + elasticSearchPort);
 			
 			transportClient.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(hostName, elasticSearchPort)));
