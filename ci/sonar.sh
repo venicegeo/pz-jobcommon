bec992404c78509e0dff055883ad06db888a8f02
Patrick D <patrick.doody@rgi-corp.com>
2018-01-03 08:24:55 -0500

    Remove deprecated CI Folder

--- ci/sonar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-root=$(pwd -P)
-mkdir -p $root/.m2/repository
-
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test org.jacoco:jacoco-maven-plugin:report -DdataFile=$root/target/jacoco.exec -Dmaven.repo.local="$root/.m2/repository"
--- ci/vars.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-APP=pz-jobcommon
-EXT=jar
